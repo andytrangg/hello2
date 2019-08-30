529c55f77123ca0d5f2d7f61363f2b3d0fe961ec
andytrangg <54644729+andytrangg@users.noreply.github.com>
2019-08-29 20:11:59 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Andy
+ * Date: 08/29/2019
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Andy Trang\n");
+  printf("Computer Science\n");
+  printf("Jake Barry/n");
+  printf("Electrical Engineering/n");
+  return 0;
+}
